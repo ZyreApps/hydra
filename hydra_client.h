127f34c07416af2f47feacf7a30e18f8f2b18701
Pieter Hintjens <ph@imatix.com>
2014-10-30 19:38:00 +0100

    Problem: need basic client and server stack
    Solution: generate client and server from simple models.
    
    - hydra_server.xml is the server model
    - hydra_client.xml is the client model
    
    Note, not yet tested.

---
+++ hydra_client.h
@@ -0,0 +1,115 @@
+/*  =========================================================================
+    hydra_client - Hydra Client
+
+    ** WARNING *************************************************************
+    THIS SOURCE FILE IS 100% GENERATED. If you edit this file, you will lose
+    your changes at the next build cycle. This is great for temporary printf
+    statements. DO NOT MAKE ANY CHANGES YOU WISH TO KEEP. The correct places
+    for commits are:
+
+     * The XML model used for this code generation: hydra_client.xml, or
+     * The code generation script that built this file: zproto_client_c
+    ************************************************************************
+    Copyright (c) the Contributors as noted in the AUTHORS file.       
+    This file is part of zbroker, the ZeroMQ broker project.           
+                                                                       
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
+    =========================================================================
+*/
+
+#ifndef __HYDRA_CLIENT_H_INCLUDED__
+#define __HYDRA_CLIENT_H_INCLUDED__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  Opaque class structure
+typedef struct _hydra_client_t hydra_client_t;
+
+//  @interface
+//  Create a new hydra_client
+//  Connect to server endpoint, with specified timeout in msecs (zero means wait    
+//  forever). Constructor succeeds if connection is successful.                     
+hydra_client_t *
+    hydra_client_new (const char *endpoint, int timeout);
+
+//  Destroy the hydra_client
+void
+    hydra_client_destroy (hydra_client_t **self_p);
+
+//  Enable verbose logging of client activity
+void
+    hydra_client_verbose (hydra_client_t *self);
+
+//  Return actor for low-level command control and polling
+zactor_t *
+    hydra_client_actor (hydra_client_t *self);
+
+//  Get list of tags from peer                                                      
+//  Returns >= 0 if successful, -1 if interrupted.
+int
+    hydra_client_get_tags (hydra_client_t *self);
+
+//  Fetch latest post for a given tag                                               
+//  Returns >= 0 if successful, -1 if interrupted.
+int
+    hydra_client_get_tag (hydra_client_t *self, const char *tag);
+
+//  Fetch a specific post byt ID                                                    
+//  Returns >= 0 if successful, -1 if interrupted.
+int
+    hydra_client_get_post (hydra_client_t *self, const char *post_id);
+
+//  Return last received status
+int 
+    hydra_client_status (hydra_client_t *self);
+
+//  Return last received reason
+char *
+    hydra_client_reason (hydra_client_t *self);
+
+//  Return last received tags
+char *
+    hydra_client_tags (hydra_client_t *self);
+
+//  Return last received tag
+char *
+    hydra_client_tag (hydra_client_t *self);
+
+//  Return last received post_id
+char *
+    hydra_client_post_id (hydra_client_t *self);
+
+//  Return last received reply_to
+char *
+    hydra_client_reply_to (hydra_client_t *self);
+
+//  Return last received previous
+char *
+    hydra_client_previous (hydra_client_t *self);
+
+//  Return last received timestamp
+int 
+    hydra_client_timestamp (hydra_client_t *self);
+
+//  Return last received type
+char *
+    hydra_client_type (hydra_client_t *self);
+
+//  Return last received content
+char *
+    hydra_client_content (hydra_client_t *self);
+
+//  Self test of this class
+void
+    hydra_client_test (bool verbose);
+//  @end
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ hydra_server.h
@@ -0,0 +1,88 @@
+/*  =========================================================================
+    hydra_server - Hydra Server (in C)
+
+    ** WARNING *************************************************************
+    THIS SOURCE FILE IS 100% GENERATED. If you edit this file, you will lose
+    your changes at the next build cycle. This is great for temporary printf
+    statements. DO NOT MAKE ANY CHANGES YOU WISH TO KEEP. The correct places
+    for commits are:
+
+     * The XML model used for this code generation: hydra_server.xml, or
+     * The code generation script that built this file: zproto_server_c
+    ************************************************************************
+    Copyright (c) the Contributors as noted in the AUTHORS file.       
+    This file is part of zbroker, the ZeroMQ broker project.           
+                                                                       
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
+    =========================================================================
+*/
+
+#ifndef __HYDRA_SERVER_H_INCLUDED__
+#define __HYDRA_SERVER_H_INCLUDED__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//  @interface
+//  To work with hydra_server, use the CZMQ zactor API:
+//
+//  Create new hydra server (in c) instance, passing logging prefix:
+//
+//      zactor_t *hydra_server__in_c_ = zactor_new (hydra_server, "myname");
+//  
+//  Destroy hydra server (in c) instance
+//
+//      zactor_destroy (&hydra_server__in_c_);
+//  
+//  Enable verbose logging of commands and activity:
+//
+//      zstr_send (hydra_server__in_c_, "VERBOSE");
+//
+//  Bind hydra server (in c) to specified endpoint. TCP endpoints may specify
+//  the port number as "*" to aquire an ephemeral port:
+//
+//      zstr_sendx (hydra_server__in_c_, "BIND", endpoint, NULL);
+//
+//  Return assigned port number, specifically when BIND was done using an
+//  an ephemeral port:
+//
+//      zstr_sendx (hydra_server__in_c_, "PORT", NULL);
+//      char *command, *port_str;
+//      zstr_recvx (hydra_server__in_c_, &command, &port_str, NULL);
+//      assert (streq (command, "PORT"));
+//
+//  Specify configuration file to load, overwriting any previous loaded
+//  configuration file or options:
+//
+//      zstr_sendx (hydra_server__in_c_, "CONFIGURE", filename, NULL);
+//
+//  Set configuration path value:
+//
+//      zstr_sendx (hydra_server__in_c_, "SET", path, value, NULL);
+//    
+//  Send zmsg_t instance to hydra server (in c):
+//
+//      zactor_send (hydra_server__in_c_, &msg);
+//
+//  Receive zmsg_t instance from hydra server (in c):
+//
+//      zmsg_t *msg = zactor_recv (hydra_server__in_c_);
+//
+//  This is the hydra_server constructor as a zactor_fn:
+//
+void
+    hydra_server (zsock_t *pipe, void *args);
+
+//  Self test of this class
+void
+    hydra_server_test (bool verbose);
+//  @end
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
