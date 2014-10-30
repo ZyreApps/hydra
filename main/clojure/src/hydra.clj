92c6be8d1358e68ce60e641a1c2c690f5f5005b7
Maximilian Karasz <maximilian.karasz@googlemail.com>
2014-10-30 20:44:47 +0100

    Problem: client can just say 'hello'.

--- main/clojure/src/hydra.clj
+++ main/clojure/src/hydra.clj
@@ -1,14 +1,38 @@
 (ns hydra
   (:require [zeromq.zmq :as zmq])
-  (:import [org.zproto HydraMsg]))
+  (:import [org.zproto HydraMsg]
+           [org.zeromq ZMsg]
+           [java.io StringWriter PrintWriter PrintStream OutputStreamWriter]))
 
 
-(defn simple-hello
-  "send a hello message to endpoint and receive the reply."
-  [endpoint]
+(defprotocol HydraClient
+  (hello    [this])
+  (get-tags [this])
+  (get-tag [this tag])
+  (get-post [this post-id])
+  (goodbye [this]))
+
+(defrecord Client [socket]
+  HydraClient
+  (hello [this]
+    (HydraMsg/sendHello socket)
+    (HydraMsg/recv socket))
+  (get-tags [this]
+    (HydraMsg/sendGet_Tags socket)
+    (HydraMsg/recv socket))
+  (get-tag [this tag]
+    (HydraMsg/sendGet_Tag socket tag)
+    (HydraMsg/recv socket))
+  (get-post [this post-id]
+    (HydraMsg/sendGet_Post socket post-id)
+    (HydraMsg/recv socket))
+  (goodbye [this]
+    (HydraMsg/sendGoodbye socket)
+    (HydraMsg/recv socket)))
+
+(defn mk-client [endpoint]
   (let [context (zmq/context)
-        output (doto (zmq/socket context :dealer)
-                 (zmq/connect endpoint)
-                 (zmq/set-receive-timeout 1000))]
-    (HydraMsg/sendHello output)
-    (HydraMsg/recv output)))
+        socket (doto (zmq/socket context :dealer)
+                 (zmq/set-receive-timeout 1000)
+                 (zmq/connect endpoint))]
+    (->Client socket)))
