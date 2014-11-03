d7a9796c2d3396836d993e226cc3e7b31f658174
Maximilian Karasz <maximilian.karasz@googlemail.com>
2014-11-03 21:06:59 +0100

    update project.clj as per template.

--- project.clj
+++ project.clj
@@ -20,8 +20,6 @@
 (defproject hydra-msg "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :java-source-paths ["main/java"]
   :source-paths ["main/clojure/src"]
   :test-paths ["main/clojure/test"]
