be68d28d8d682d61201755b8794cbf69f007adfd
Pieter Hintjens <ph@imatix.com>
2014-11-07 10:05:50 +0100

    Problem: out of sync with latest zproject
    Solution: regenerate the project files
    
    Note: fails on man page generation, to be fixed asap.

---
+++ include/hydra_library.h
@@ -0,0 +1,67 @@
+/*  =========================================================================
+    hydra - HYDRA wrapper
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.       
+    This file is part of zbroker, the ZeroMQ broker project.           
+                                                                       
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
+
+    #################################################################
+    #   GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY    #
+    #   Please read the README.txt file                             #
+    #################################################################
+    =========================================================================
+*/
+
+#ifndef __HYDRA_LIBRARY_H_INCLUDED__
+#define __HYDRA_LIBRARY_H_INCLUDED__
+
+//  External dependencies
+#include <czmq.h>
+
+//  HYDRA version macros for compile-time API detection
+
+#define HYDRA_VERSION_MAJOR 0
+#define HYDRA_VERSION_MINOR 1
+#define HYDRA_VERSION_PATCH 0
+
+#define HYDRA_MAKE_VERSION(major, minor, patch) \
+    ((major) * 10000 + (minor) * 100 + (patch))
+#define HYDRA_VERSION \
+    HYDRA_MAKE_VERSION(HYDRA_VERSION_MAJOR, HYDRA_VERSION_MINOR, HYDRA_VERSION_PATCH)
+
+#if defined (__WINDOWS__)
+#   if defined LIBHYDRA_STATIC
+#       define HYDRA_EXPORT
+#   elif defined LIBHYDRA_EXPORTS
+#       define HYDRA_EXPORT __declspec(dllexport)
+#   else
+#       define HYDRA_EXPORT __declspec(dllimport)
+#   endif
+#else
+#   define HYDRA_EXPORT
+#endif
+
+//  Opaque class structures to allow forward references
+typedef struct _hydra_msg_t hydra_msg_t;
+#define HYDRA_MSG_T_DEFINED
+typedef struct _hydra_server_t hydra_server_t;
+#define HYDRA_SERVER_T_DEFINED
+typedef struct _hydra_client_t hydra_client_t;
+#define HYDRA_CLIENT_T_DEFINED
+
+
+//  Public API classes
+#include "hydra_msg.h"
+#include "hydra_server.h"
+#include "hydra_client.h"
+
+#endif
+/*
+    #################################################################
+    #   GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY    #
+    #   Please read the README.txt file in the model directory.     #
+    #################################################################
+*/    
